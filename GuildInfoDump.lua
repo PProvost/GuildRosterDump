28f29aa4c2481fe656d8f9418b0ebdb5d3dfa42c
PProvost <peter@provost.org>
2009-10-12 17:13:50 -0600

    Rewrote to stop using savedvars. Still having issues with the GuildRoster() throttling.

--- GuildInfoDump.lua
+++ GuildInfoDump.lua
@@ -15,81 +15,78 @@ limitations under the License.
 ]]
 
 local FREQUENCY = 5 * 60 -- 5 mins
-local defaults, db = {}
 
 local function Print(...) print("|cFF33FF99GuildInfoDump|r:", ...) end
 local debugf = tekDebug and tekDebug:GetFrame("GuildInfoDump")
 local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
 
-local f = CreateFrame("frame")
+-- Setup the main frame
+local f = CreateFrame("frame", "GuildInfoDumpFrame", UIParent)
+
+f:SetBackdrop( {
+	bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
+	edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
+	tile = true, tileSize = 16, edgeSize = 16, 
+	insets = { left = 4, right = 4, top = 4, bottom = 4 }
+})
+f:SetBackdropColor(0,0,0,1)
+f:SetHeight(300)
+f:SetWidth(500)
+f:SetPoint("CENTER", UIParent, "CENTER")
+local scrollEdit = LibStub("QScrollingEditBox"):New("GuildInfoDumpEditBox", f)
+scrollEdit:SetPoint("TOPLEFT", 5, -5)
+scrollEdit:SetPoint("BOTTOMRIGHT", -5, 5)
+f:Hide()
+
 f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
 f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("GUILD_ROSTER_UPDATE")
 
+table.insert(UISpecialFrames, "GuildInfoDumpFrame")
+
 function f:ADDON_LOADED(event, addon)
 	if addon:lower() ~= "guildinfodump" then return end
-	GuildInfoDumpDB = setmetatable(GuildInfoDumpDB or {}, {__index = defaults})
-	db = GuildInfoDumpDB
 	LibStub("tekKonfig-AboutPanel").new(nil, "GuildInfoDump") -- Make first arg nil if no parent config panel
 	self:UnregisterEvent("ADDON_LOADED"); self.ADDON_LOADED = nil
 	if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterEvent("PLAYER_LOGIN") end
 end
 
 function f:PLAYER_LOGIN()
-	GuildRoster()
 	self:UnregisterEvent("PLAYER_LOGIN"); self.PLAYER_LOGIN = nil
 end
 
 -- We've got data!
+local csvData
 function f:GUILD_ROSTER_UPDATE()
-	local guildName, guildRankName, guildRankIndex = GetGuildInfo("player")
-	db.GuildInfo = {
-		Name = select(1, GetGuildInfo("player")),
-		GuildInfo = GetGuildInfoText(),
-		MOTD = GetGuildRosterMOTD(),
-	}
+	if not f:IsShown() then return end
 
 	local name, rank, rankIndex, level, class, zone, note, officernote, online, status
 	local years, months, days, hours
+	local lastOnline
 
-	db.Roster = {}
+	csvData = "Name,Rank,Level,Class,Zone,Note,OfficerNote,LastOnline,Status\n"
+
 	for index = 1,GetNumGuildMembers(true) do
 		name, rank, rankIndex, level, class, zone, note, officernote, online, status = GetGuildRosterInfo(index)
 		years, months, days, hours = GetGuildRosterLastOnline(index)
 
-		local lastOnline = "Now"
 		if not online then
 			lastOnline = string.format("%d years, %d months, %d days, %d hours", years, months, days, hours)
+		else
+			lastOnline = "Now"
 		end
 
-		db.Roster[name] = {
-			Rank = rank,
-			Level = level,
-			Class = class,
-			Zone = zone,
-			Note = note,
-			OfficerNote = officernote,
-			LastOnline = lastOnline,
-			Status = status
-		}
+		csvData = csvData .. string.format("%s,%s,%s,%s,%s,%s,%s,%s,%s\n", name, rank, level, class, zone, note, officernote or "", lastOnline, status)
 	end
-end
 
--- Mini-timer to rescan periodically
---[[
-local total = 0
-local function OnUpdate(self, elapsed)
-	total = total + elapsed
-	if total >= FREQUENCY then
-		GuildRoster()
-	end
+	scrollEdit:SetText(csvData)
+	csvData = ""
 end
-f:SetScript("OnUpdate", onUpdate)
-]]
 
 -- Slash command to force the scan
-SLASH_ADDONTEMPLATE1 = "/gdump"
-SlashCmdList.ADDONTEMPLATE = function(msg)
+SLASH_GUILDINFODUMP1 = "/gdump"
+SlashCmdList.GUILDINFODUMP = function(msg)
+	f:Show()
 	GuildRoster()
 end
 
