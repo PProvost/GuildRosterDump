e5c3ad0d232603f8e8109ea93bfcb45541e0c759
PProvost <peter@provost.org>
2009-10-13 13:19:32 -0600

    Minor frame tweaking for usability

--- GuildRosterDump.lua
+++ GuildRosterDump.lua
@@ -14,32 +14,53 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ]]
 
-local delim = ","
+local delim = ";"
 local rosterData 
 
-local function Print(...) print("|cFF33FF99GuildInfoDump|r:", ...) end
-local debugf = tekDebug and tekDebug:GetFrame("GuildInfoDump")
+local function Print(...) print("|cFF33FF99GuildRosterDump|r:", ...) end
+local debugf = tekDebug and tekDebug:GetFrame("GuildRosterDump")
 local function Debug(...) if debugf then debugf:AddMessage(string.join(", ", tostringall(...))) end end
 
 -- Setup the main frame
-local f = CreateFrame("frame", "GuildInfoDumpFrame", UIParent)
+local f = CreateFrame("frame", "GuildRosterDumpFrame", UIParent)
 
-f:SetBackdrop( {
+f:SetBackdrop({
 	bgFile = "Interface/Tooltips/UI-Tooltip-Background", 
 	edgeFile = "Interface/Tooltips/UI-Tooltip-Border", 
 	tile = true, tileSize = 16, edgeSize = 16, 
-	insets = { left = 4, right = 4, top = 4, bottom = 4 }
+	insets = { left = 4, right = 4, top = 4, bottom = 4 },
 })
 f:SetBackdropColor(0,0,0,1)
-f:SetHeight(300)
+f:SetHeight(350)
 f:SetWidth(500)
 f:SetPoint("CENTER", UIParent, "CENTER")
-local scrollEdit = LibStub("QScrollingEditBox"):New("GuildInfoDumpEditBox", f)
-scrollEdit:SetPoint("TOPLEFT", 5, -5)
-scrollEdit:SetPoint("BOTTOMRIGHT", -5, 5)
+f:SetMovable(true)
+f:EnableMouse(true)
+f:SetFrameStrata("DIALOG")
+f:SetScript("OnMouseDown", function(self) self:StartMoving() end)
+f:SetScript("OnMouseUp", function(self) self:StopMovingOrSizing() end)
+
+local closeBtn = CreateFrame("Button", nil, f, "UIPanelCloseButton")
+closeBtn:SetWidth(32); closeBtn:SetHeight(32);
+closeBtn:SetPoint("TOPRIGHT", -2, -2)
+closeBtn:SetScript("OnClick", function(self) f:Hide() end)
+
+local scrollEdit = LibStub("QScrollingEditBox"):New("GuildRosterDumpEditBox", f)
+scrollEdit:SetPoint("TOPLEFT", 4, -40)
+scrollEdit:SetPoint("BOTTOMRIGHT", -30, 8)
 f:Hide()
 
-table.insert(UISpecialFrames, "GuildInfoDumpFrame")
+local title = f:CreateFontString(nil, "OVERLAY", "GameFontNormalLarge")
+title:SetPoint("TOPLEFT", 8, -8)
+title:SetText("GuildRosterDump")
+
+local description = f:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
+description:SetPoint("TOPLEFT", title, "BOTTOMLEFT")
+description:SetPoint("BOTTOMRIGHT", scrollEdit, "TOPRIGHT")
+description:SetJustifyH("LEFT"); description:SetJustifyV("TOP")
+description:SetText("Copy the following text to the clipboard and paste it into your external program (e.g. Excel).")
+
+table.insert(UISpecialFrames, "GuildRosterDumpFrame")
 f:SetScript("OnEvent", function(self, event, ...) if self[event] then return self[event](self, event, ...) end end)
 f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("GUILD_ROSTER_UPDATE")
@@ -51,14 +72,9 @@ f:SetScript("OnShow", function()
 end)
 
 function f:ADDON_LOADED(event, addon)
-	if addon:lower() ~= "guildinfodump" then return end
-	LibStub("tekKonfig-AboutPanel").new(nil, "GuildInfoDump") -- Make first arg nil if no parent config panel
+	if addon:lower() ~= "guildrosterdump" then return end
+	LibStub("tekKonfig-AboutPanel").new(nil, "GuildRosterDump") -- Make first arg nil if no parent config panel
 	self:UnregisterEvent("ADDON_LOADED"); self.ADDON_LOADED = nil
-	if IsLoggedIn() then self:PLAYER_LOGIN() else self:RegisterEvent("PLAYER_LOGIN") end
-end
-
-function f:PLAYER_LOGIN()
-	self:UnregisterEvent("PLAYER_LOGIN"); self.PLAYER_LOGIN = nil
 end
 
 -- We've got data!
@@ -83,13 +99,13 @@ function f:GUILD_ROSTER_UPDATE()
 			lastOnline = "Now"
 		end
 
-		table.insert(rosterData, strjoin(delim, name,rank,level,class,zone,note,(officernote or ""),lastOnline,status))
+		table.insert(rosterData, strjoin(delim, name or "", rank or "", level or 0, class or "", zone or "", note or "", officernote or "", lastOnline, status or ""))
 	end
 end
 
 -- Slash command to force the scan
-SLASH_GUILDINFODUMP1 = "/gdump"
-SlashCmdList.GUILDINFODUMP = function(msg)
+SLASH_GUILDROSTERDUMP1 = "/gdump"
+SlashCmdList.GUILDROSTERDUMP = function(msg)
 	GuildRoster()
 	f:Show()
 end
